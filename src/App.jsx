a9b36f7dc39b2822c89865c136813bad6e83f5c1
Raktimabho Ghosh <raktimabhoghosh@Raktimabhos-MacBook-Air.local>
2025-03-19 00:12:22 +0530

    Setting up the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Body from "./Body";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+
 function App() {
 
   return (
     <>
-      Hello Vite
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element = { <Body /> }>
+            <Route path="/login" element = { <Login /> } />
+            <Route path="/profile" element = { <Profile /> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
